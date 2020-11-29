fa01692a36936e84cbc93281e48549f6618e5cee
Apro123 <akapoor3@ucmerced.edu>
2020-11-29 11:52:01 -0800

    get sessions and output on general template page

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+import subprocess
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+def getSessions():
+    files = subprocess.getoutput("ls /Users/armaankapoor/Public/MoteOutput").split()
+    sessions = []
+    for i in range(len(files)):
+        #all sessions have at least one node
+        if(files[i].split("_")[3][0] == "1"):
+            sessions.append(int(files[i].split("_")[1]))
+    return sessions
+
+@app.route('/session/<num>')
+def getSessionData(num=None):
+    return render_template("session.html", sess=num)
+
+@app.route('/', methods=['GET'])
+def getIndex():
+    return render_template("index.html", sessions=getSessions())
 
 
 if __name__ == '__main__':
