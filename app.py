9d1e2bafcd5edf9422fb156db8e39235ae5df2b3
Apro123 <akapoor3@ucmerced.edu>
2020-12-11 00:23:33 -0800

    time series graphs work now. working solution

--- app.py
+++ app.py
@@ -7,14 +7,61 @@ import os
 
 app = Flask(__name__)
 
-numFiles = len(subprocess.getoutput("ls /Users/armaankapoor/Public/MoteOutput").split())
+files = subprocess.getoutput("ls /Users/armaankapoor/Public/MoteOutput").split()
+
+@app.route('/getInterval/<min>/<max>/', methods=['GET'])
+def interval(min, max):
+    min = int(min)
+    max = int(max)
+    # print(min)
+    # print(max)
+    tsdata = [] # { from:  to: node, data: [timestamp, link]
+    for i in range(len(files)):
+        # try:
+            with open("/Users/armaankapoor/Public/MoteOutput/"+files[i], "r") as f:
+                for line in f.readlines():
+                    try:
+                        date = int(float(line.split(" ")[0]) * 1000)
+                    except:
+                        date = 0
+
+                    if(date != 0):
+                        if(date >= min and date <= max):
+                            restOfData = line.split(" ")[1].split(",")
+
+                            found = False
+                            for j in range(len(tsdata)):
+                                if(int(tsdata[j]['from']) == int(restOfData[1]) and int(tsdata[j]['to']) == int(restOfData[2])):
+                                    tsdata[j]['data'].append([date, int(restOfData[3].strip())])
+                                    found = True
+                            if(not found):
+                                tsdata.append({"from": int(restOfData[1]), "to": int(restOfData[2]), "data": [[date, int(restOfData[3].strip())]]})
+                            # tsdata.append([date, int(restOfData[1]), int(restOfData[2]), int(restOfData[3].strip())])
+                        elif(date > max):
+                            break
+        # except:
+        #     pass
+    return Response(json.dumps(tsdata), content_type='application/json')
+
+@app.route('/getMinDate', methods=['GET'])
+def minDate():
+    minDt = 9999999999999
+    for i in range(len(files)):
+        try:
+            with open("/Users/armaankapoor/Public/MoteOutput/"+files[i], "r") as f:
+                date = float(f.readline().split(" ")[0]) * 1000
+                if(i == 0 or (date != 0 and minDt > date)):
+                    minDt = date
+        except:
+            pass
+    return Response(str(minDt), content_type='application/json')
 
 @app.route('/stream', methods=['GET'])
 def getDataStream():
     def stream():
         fileDes = []
-        for i in range(numFiles):
-            file = open("/Users/armaankapoor/Public/MoteOutput/Node_" + str(i+1) + ".txt", "r")
+        for i in range(len(files)):
+            file = open("/Users/armaankapoor/Public/MoteOutput/"+files[i], "r")
             file.seek(0, 2)
             try:
                 file.seek(file.tell() - 24 * 500, os.SEEK_SET)
