0fb4ecdfcb1621e9afb8aae88561c26a7184b410
Apro123 <akapoor3@ucmerced.edu>
2020-12-10 17:47:50 -0800

    remove not working, stream working, nodes and table of data updates as stream of data incoming

--- app.py
+++ app.py
@@ -1,43 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import subprocess
 import json
+from datetime import datetime
+from time import sleep
+import os
 
 app = Flask(__name__)
 
-def getSessions():
-    files = subprocess.getoutput("ls /Users/armaankapoor/Public/MoteOutput").split()
-    sessions = {}
-    for i in range(len(files)):
-        #all sessions have at least one node
-        if(files[i].split("_")[3][0] == "1"):
-            sessions[(int(files[i].split("_")[1]))] = 1
-        else:
-            sessions[(int(files[i].split("_")[1]))] += 1
-    return sessions
+numFiles = len(subprocess.getoutput("ls /Users/armaankapoor/Public/MoteOutput").split())
 
-def getSessionData(sessNum):
-    data = [
-        # {nodeNumber: 3,
-        # data: ["time 1,2,3,4", "time 1,2,3,4", etc...]}
-    ]
-    #
-    for i in range(sessions[int(sessNum)]):
-        with open("static/Session_" + sessNum + "_Node_" + str(i+1) + ".txt") as f:
-            data.append({"node": i+1, "data": f.readlines()})
-    return data
-
-sessions = getSessions()
-
-@app.route('/session/<sessNum>', methods=['GET'])
-def getSession(sessNum=None):
-    #serve static processed data files
-    d = getSessionData(sessNum)
-    return render_template("session.html", sess=sessNum, data=json.dumps(d))
+@app.route('/stream', methods=['GET'])
+def getDataStream():
+    def stream():
+        fileDes = []
+        for i in range(numFiles):
+            file = open("/Users/armaankapoor/Public/MoteOutput/Node_" + str(i+1) + ".txt", "r")
+            file.seek(0, 2)
+            try:
+                file.seek(file.tell() - 24 * 500, os.SEEK_SET)
+            except:
+                file.seek(0)
+            fileDes.append(file)
+        while True:
+            dataToReturn = ""
+            for f in fileDes:
+                data = f.readline()
+                if(data != ""):
+                    dataToReturn += data
+            yield dataToReturn
+    return Response(stream(), content_type='application/json')
 
 @app.route('/', methods=['GET'])
 def getIndex():
-    return render_template("index.html", sessions=sessions)
+    # file = open("/Users/armaankapoor/Public/MoteOutput/Node_4.txt", 'r')
+    # print("-~-")
+    # file.seek(0,2)
+    # try:
+    #     file.seek(file.tell() - 24*500, os.SEEK_SET)
+    # except:
+    #     file.seek(0)
+    # print(file.readlines())
+    # print("---")
+    # return "4"
+    return render_template("index.html")
 
+
+#print(os.stat("/Users/armaankapoor/Public/MoteOutput/Node_4.txt").st_size/24); #number of lines
 
 if __name__ == '__main__':
     app.run(debug=True)
